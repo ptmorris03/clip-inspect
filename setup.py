7282fe3b1ca1fa284627416cb42e126d9554eadf
Paul Morris <pmorris2012@fau.edu>
2021-06-22 13:50:31 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='clip-inspect',
+   version='0.1',
+   description='pay no mind :)',
+   author='Paul Morris',
+   author_email='pmorris2012@fau.edu',
+   packages=['clip-inspect'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
