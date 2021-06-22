95653ed9644269236573f40359761d4ec7da5aa1
Paul Morris <pmorris2012@fau.edu>
2021-06-22 13:51:55 -0400

    name fix

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='clip-inspect',
+   name='clip_inspect',
    version='0.1',
    description='pay no mind :)',
    author='Paul Morris',
    author_email='pmorris2012@fau.edu',
-   packages=['clip-inspect'],  #same as name
+   packages=['clip_inspect'],  #same as name
    install_requires=[], #external packages as dependencies
 )
