84dfa81aced5dabf09cc2dcf5651209e5b1f92c9
pmorris2012 <pmorris2012@fau.edu>
2021-08-06 00:47:21 -0400

    docker refactor

---
+++ docker/devel_push.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker cp . clip-inspect:/clip-inspect
--- docker/run_script.sh
+++ docker/run_script.sh
@@ -4,6 +4,6 @@ docker run \
     -it --rm --init \
     --shm-size=16G \
     --gpus all \
-    -v /media/mpcrpaul/fastdata/colab:/data \
     -v $(pwd):/code \
-    colab-local-runtime python3 /code/"$@"
+    --name "clip-inspect"
+    pmorris2012/clip-inspect:jax-base python3 /code/"$@"
