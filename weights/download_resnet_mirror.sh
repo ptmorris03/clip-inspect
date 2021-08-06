742a838131e83df83758837dab6a0e07bde5c917
pmorris2012 <pmorris2012@fau.edu>
2021-08-05 21:26:48 -0400

    fixed mirror scripts

--- weights/download_resnet_mirror.sh
+++ weights/download_resnet_mirror.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl https://github.com/pmorris2012/clip-inspect/releases/download/weights/RN50.pt -o RN50.pt
-curl https://github.com/pmorris2012/clip-inspect/releases/download/weights/RN101.pt -o RN101.pt
-curl https://github.com/pmorris2012/clip-inspect/releases/download/weights/RN50x4.pt -o RN50x4.pt
-curl https://github.com/pmorris2012/clip-inspect/releases/download/weights/RN50x16.pt -o RN50x16.pt
+curl https://github.com/pmorris2012/clip-inspect/releases/download/weights/RN50.pt -O RN50.pt
+curl https://github.com/pmorris2012/clip-inspect/releases/download/weights/RN101.pt -O RN101.pt
+curl https://github.com/pmorris2012/clip-inspect/releases/download/weights/RN50x4.pt -O RN50x4.pt
+curl https://github.com/pmorris2012/clip-inspect/releases/download/weights/RN50x16.pt -O RN50x16.pt
--- weights/download_vit_mirror.sh
+++ weights/download_vit_mirror.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl https://github.com/pmorris2012/clip-inspect/releases/download/weights/ViT-B-32.pt -o ViT-B-32.pt
-curl https://github.com/pmorris2012/clip-inspect/releases/download/weights/ViT-B-16.pt -o ViT-B-16.pt
+wget https://github.com/pmorris2012/clip-inspect/releases/download/weights/ViT-B-32.pt -O ViT-B-32.pt
+wget https://github.com/pmorris2012/clip-inspect/releases/download/weights/ViT-B-16.pt -O ViT-B-16.pt
 
