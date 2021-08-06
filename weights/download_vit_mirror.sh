f651a2a48bfb47f5c48a0d57b6ae917acc725e92
pmorris2012 <pmorris2012@fau.edu>
2021-08-05 21:10:36 -0400

    added mirror link for weights

---
+++ weights/download_vit_mirror.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl https://github.com/pmorris2012/clip-inspect/releases/download/weights/ViT-B-32.pt -o ViT-B-32.pt
+curl https://github.com/pmorris2012/clip-inspect/releases/download/weights/ViT-B-16.pt -o ViT-B-16.pt
+
