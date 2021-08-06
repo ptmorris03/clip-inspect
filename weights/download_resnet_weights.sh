cf5aae95ba16f539ac38cb7a934c1b6170fb23b2
pmorris2012 <pmorris2012@fau.edu>
2021-08-05 20:18:59 -0400

    weight download scripts

---
+++ weights/download_resnet_weights.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl https://openaipublic.azureedge.net/clip/models/afeb0e10f9e5a86da6080e35cf09123aca3b358a0c3e3b6c78a7b63bc04b6762/RN50.pt -o RN50.pt
+curl https://openaipublic.azureedge.net/clip/models/8fa8567bab74a42d41c5915025a8e4538c3bdbe8804a470a72f30b0d94fab599/RN101.pt -o RN101.pt
+curl https://openaipublic.azureedge.net/clip/models/7e526bd135e493cef0776de27d5f42653e6b4c8bf9e0f653bb11773263205fdd/RN50x4.pt -o RN50x4.pt
+curl https://openaipublic.azureedge.net/clip/models/52378b407f34354e150460fe41077663dd5b39c54cd0bfd2b27167a4a06ec9aa/RN50x16.pt -o RN50x16.pt
---
+++ weights/download_vit_weights.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt -o ViT-B-32.pt
+curl https://openaipublic.azureedge.net/clip/models/5806e77cd80f8b59890b7e101eabd078d9fb84e6937f9e85e4ecb61988df416f/ViT-B-16.pt -o ViT-B-16.pt
