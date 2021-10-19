3181797e547da9c2c09afdc16e1f1c467a17f75b
pmorris2012 <pmorris2012@fau.edu>
2021-10-19 18:19:45 -0400

    stratified iterator

--- clip_inspect/generate.py
+++ clip_inspect/generate.py
@@ -1,16 +1,46 @@
 import jax.numpy as jnp
 import jax
+from itertools import product
 
 
 def random_points(prng_key, count, dims):
     return jax.random.normal(prng_key, (count, dims))
 
 
-def mesh1d_points(count, batch_size):
-    assert count % batch_size == 0
+def mesh_line1d(density=300, limit=1):
+    if type(limit) is int:
+        limit = [-limit, limit]
 
-    return NotImplementedError
+    return jnp.linspace(limit[0], limit[1], density)
 
 
-def mesh2d_points(*args):
-    return NotImplementedError
+def mesh_plane2d(density=300, limit=1):
+    if type(limit) is int:
+        limit = [[-limit, limit], [-limit, limit]]
+
+    points = []
+    for (l1, l2) in limit:
+        points.append(jnp.linspace(l1, l2, density))
+
+    return jnp.stack(jnp.meshgrid(*points), axis=-1)
+
+
+def stratified_iterator(tensor, num_batches, axis=None):
+    if axis is None:
+        axis = list(range(len(tensor.shape) - 1))
+    if type(axis) is int:
+        axis = [axis]
+
+    ranges = [range(num_batches)] * len(axis)
+    for idxs in product(*ranges):
+        slices = []
+        axis_counter = 0
+        for t_idx in range(len(tensor.shape)):
+            if t_idx in axis:
+                slices.append(slice(idxs[axis_counter], None, num_batches))
+                axis_counter += 1
+            else:
+                slices.append(slice(tensor.shape[t_idx]))
+        yield tuple(slices)
+    
+
--- test.py
+++ test.py
@@ -1,15 +1,34 @@
 from clip_inspect.weights import load
 from clip_inspect.model import MLP
-from clip_inspect.generate import random_points
+from clip_inspect.generate import random_points, mesh_plane2d, stratified_iterator
 from clip_inspect.transforms import pnorm, loop, residual
 import jax.numpy as jnp
+import jax
+from tqdm import tqdm
 from jax import vmap, jit
 from functools import partial
+import matplotlib.pyplot as plt
 
 
 state_dict, info = load("ViT-B-32")
 mlp = MLP(state_dict, "transformer.resblocks.0")
-points = random_points(mlp.prng_key, 1000, 512)
-points = vmap(mlp.in_project)(points)
-all_points = vmap(jit(pnorm(loop(residual(mlp.forward), 100))))(points)
-print(all_points.shape)
+#points = random_points(mlp.prng_key, 1000, 512)
+points = mesh_plane2d(density=100, limit=5)
+out = jnp.zeros(points.shape[:-1])
+for idxs in tqdm(stratified_iterator(points, 5)):
+    idxs = idxs[:-1]
+
+    batch = points[idxs]
+    batch_shape = batch.shape[:-1]
+    batch = batch.reshape(-1, points.shape[-1])
+
+    batch = vmap(mlp.in_project)(batch)
+    batch_out = vmap(jit(pnorm(loop(residual(mlp.forward), 100))))(batch)
+    
+    out = jax.ops.index_update(
+        out,
+        jax.ops.index[idxs],
+        batch_out.reshape(*batch_shape)
+    )
+
+
