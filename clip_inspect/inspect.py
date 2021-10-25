5a3708ffa37dbd627143b5bc3aecdb6d2befbf87
pmorris2012 <pmorris2012@fau.edu>
2021-10-25 17:22:44 -0400

    planning

--- clip_inspect/inspect.py
+++ clip_inspect/inspect.py
@@ -10,3 +10,10 @@ def axis_color(vector):
     probs = jax.nn.softmax(jnp.abs(vector))
     weights = jnp.linspace(0, 1, vector.shape[-1], endpoint=False)
     return (probs * weights).sum(axis=-1)
+
+
+##HUE HISTOGRAM CLASS
+#   - remembers extent (set automatically in first call)
+#   - one hist for count
+#   - corresponding hist for hue weight
+#   - to display, hue = weight / count, intensity = count
