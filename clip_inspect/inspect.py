5619c0c4c2299241650a44421a64d5459fe74845
pmorris2012 <pmorris2012@fau.edu>
2021-10-27 15:28:17 -0400

    circular mean for axis coloring

--- clip_inspect/inspect.py
+++ clip_inspect/inspect.py
@@ -7,9 +7,11 @@ def norm01(tensor):
 
 
 def axis_color(vector):
+    angles = jnp.linspace(-jnp.pi, jnp.pi, vector.shape[-1], endpoint=False)
     probs = jax.nn.softmax(jnp.abs(vector))
-    weights = jnp.linspace(0, 1, vector.shape[-1], endpoint=False)
-    return (probs * weights).sum(axis=-1)
+    xcoord = (probs * jnp.cos(angles)).sum(axis=-1)
+    ycoord = (probs * jnp.sin(angles)).sum(axis=-1)
+    return (jnp.arctan2(xcoord, ycoord) + jnp.pi) / (2 * jnp.pi)
 
 
 ##HUE HISTOGRAM CLASS
