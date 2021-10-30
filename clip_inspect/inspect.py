3918388d1ce5897a00f06a12035a385b49bc7f6a
Paul Morris <pmorris2012@fau.edu>
2021-10-30 19:05:39 -0400

    rename "Axis color" to polar_nd

--- clip_inspect/inspect.py
+++ clip_inspect/inspect.py
@@ -6,12 +6,20 @@ def norm01(tensor):
     return (tensor - tensor.min()) / (tensor.max() - tensor.min())
 
 
-def axis_color(vector):
+def magnitude(vector):
+    return jnp.linalg.norm(vector, axis=-1)
+
+
+def angle_nd(vector):
     angles = jnp.linspace(-jnp.pi, jnp.pi, vector.shape[-1], endpoint=False)
     probs = jax.nn.softmax(jnp.abs(vector))
     xcoord = (probs * jnp.cos(angles)).sum(axis=-1)
     ycoord = (probs * jnp.sin(angles)).sum(axis=-1)
-    return (jnp.arctan2(xcoord, ycoord) + jnp.pi) / (2 * jnp.pi)
+    return jnp.arctan2(xcoord, ycoord)
+
+
+def polar_nd(vector):
+    return angle_nd(vector), magnitude(vector)
 
 
 ##HUE HISTOGRAM CLASS
