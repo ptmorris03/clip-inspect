faa37ac12a29380a34cc16ab2a3a6d2afefecb9a
pmorris2012 <pmorris2012@fau.edu>
2021-10-30 21:06:08 -0400

    attractor histograms

--- experiments/long_iteration.py
+++ experiments/long_iteration.py
@@ -11,11 +11,19 @@ from functools import partial
 import matplotlib.pyplot as plt
 from pathlib import Path
 import numpy as np
+from PIL import Image
 
-n_points = 2000
+n_accum = 10
+n_points = 4000
 pre_steps = 1000
-steps = 1
-layer = 7
+steps = 10000
+layer = 10
+
+hist_bins = [1920, 1080]
+
+#for layer 7
+#hist_range = [[3.15, 4.78], [0.632, 0.705]]
+hist_range = [[2, 22], [0, 1]]
 
 state_dict, info = load("ViT-B-32", base_path="/code/weights/")
 mlp = MLP(state_dict, F"transformer.resblocks.{layer-1}")
@@ -28,33 +36,65 @@ f_forward = pmap(vmap(jit(collect(mlp.forward, f_collect))))
 f_jac = pmap(vmap(jit(jacobian(mlp.forward))))
 
 def lyapunov_dimension(J):
-    eigvals = jnp.flip(jnp.sort(jnp.linalg.eigvals(J).real))
+    eigvals = jnp.flip(jnp.sort(jnp.linalg.eigvals(J)))
     cumsum = jnp.cumsum(eigvals)
     j = (cumsum >= 0).sum()
     return j + cumsum[j - 1] / jnp.abs(eigvals[j])
 f_lldimension = vmap(jit(lyapunov_dimension, backend='cpu'))
 
-out = np.zeros((2 * n_points, steps, 2))
-points = random_points(mlp.prng_key, 2 * n_points, 512)
-points = points.reshape(2, n_points, 512)
-points = f_in(points)
-for s_idx in tqdm(range(pre_steps)):
-    points, (out_norm, out_axis) = f_forward(points)
-for s_idx in tqdm(range(steps)):
-    points, (out_norm, out_axis) = f_forward(points)
-    #J = f_jac(points).reshape(2 * n_points, 512, 512)
-    #d = f_lldimension(J)
+def hist2d(coords):
+    return jnp.histogram2d(
+        coords[...,0].reshape(-1),
+        coords[...,1].reshape(-1),
+        bins=hist_bins,
+        range=hist_range,
+        density=True
+    )[0]
+hist2d = vmap(jit(hist2d))
 
-    out[:,s_idx,0] = out_norm.reshape(-1)
-    out[:,s_idx,1] = out_axis.reshape(-1)
-    
-fig, ax = plt.subplots(figsize=(5, 5))
-c = np.linspace(0,1, steps)
-for i in range(2 * n_points):
-    plt.scatter(
-        out[i,:,0],
-        out[i,:,1],
-        s=1,
-        c=c
-    )
-fig.savefig("/code/output/long_iteration2.png")
+def batch_hist2d(coords):
+    return hist2d(coords).sum(axis=0)
+batch_hist2d = pmap(jit(batch_hist2d))
+
+key = mlp.prng_key
+hist = None
+for a_idx in tqdm(range(n_accum)):
+    key, subkey = jax.random.split(key)
+    out = np.zeros((2 * n_points, steps, 2))
+    points = random_points(subkey, 2 * n_points, 512)
+    points = points.reshape(2, n_points, 512)
+    points = f_in(points)
+    for s_idx in tqdm(range(pre_steps), leave=False):
+        points, (out_norm, out_axis) = f_forward(points)
+    for s_idx in tqdm(range(steps), leave=False):
+        points, (out_norm, out_axis) = f_forward(points)
+        #J = f_jac(points).reshape(2 * n_points, 512, 512)
+        #d = f_lldimension(J)
+
+        out[:,s_idx,0] = out_norm.reshape(-1)
+        out[:,s_idx,1] = out_axis.reshape(-1)
+    out = out.reshape(2, -1, 1000, steps, 2)
+
+    print(out.reshape(-1,2).min(axis=0), out.reshape(-1,2).max(axis=0))
+
+    _hist = batch_hist2d(out).sum(axis=0)
+    if hist is None:
+        hist = _hist
+    else:
+        hist += _hist
+
+hist_norm = norm01(jnp.maximum(0, jnp.log(hist.T + 1e-9)))
+im = (plt.cm.inferno(hist_norm)[...,:3] * 255.99).astype(np.uint8)
+im = Image.fromarray(im)
+im.save(F"/code/output/map_attractor_layer{layer}.png")
+
+#fig, ax = plt.subplots(figsize=(20, 20))
+#c = np.linspace(0,1, steps)
+#for i in range(2 * n_points):
+#    plt.scatter(
+#        out[i,:,0],
+#        out[i,:,1],
+#        s=.1,
+#        #c=c
+#    )
+#fig.savefig("/code/output/long_iteration2.png")
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from clip_inspect.weights import load
 from clip_inspect.model import MLP
 from clip_inspect.generate import random_points, mesh_plane2d, stratified_iterator
-from clip_inspect.transforms import pnorm, loop, residual, loop_collect_residual
+from clip_inspect.transforms import pnorm, residual, collect
 from clip_inspect.inspect import norm01
 import jax.numpy as jnp
 import jax
@@ -14,37 +14,55 @@ from skimage.io import imsave
 import numpy as np
 
 density = 2160
-num_batches = 20
+num_batches = 40
 limit = 3
 steps = 300
 layer = 8
 
+out_dir = Path(F"/code/output/")
+workspace_dir = Path(out_dir, "workspace/")
+
+template_name = F"plane2d_2norm_{layer}_{density}_{steps}"
+    
+out_dir.mkdir(parents=True, exist_ok=True)
+workspace_dir.mkdir(parents=True, exist_ok=True)
 state_dict, info = load("ViT-B-32", base_path="/code/weights/")
 mlp = MLP(state_dict, F"transformer.resblocks.{layer-1}")
 #points = random_points(mlp.prng_key, 1000, 512)
 points = mesh_plane2d(density=density, limit=limit)
-out = jax.device_put(jnp.zeros((*points.shape[:-1], steps)), jax.devices("cpu")[0])
+out = np.zeros(points.shape[:-1])
+for frame_idx in range(steps):
+    array_path = Path(workspace_dir, F"{template_name}_{frame_idx:06d}.npy")
+    np.save(array_path, out)
+
+f_in = vmap(mlp.in_project)
+f_step = vmap(jit(collect(residual(mlp.forward), lambda x: jnp.linalg.norm(x, axis=-1))))
+
 for idxs in tqdm(stratified_iterator(points, num_batches)):
     idxs = idxs[:-1]
 
     batch = points[idxs]
     batch_shape = batch.shape[:-1]
     batch = batch.reshape(-1, points.shape[-1])
+    batch = f_in(batch)
 
-    batch = vmap(mlp.in_project)(batch)
-    batch_out = vmap(jit(loop_collect_residual(mlp.forward, steps, lambda x: jnp.linalg.norm(x, axis=-1))))(batch)
-    
-    out = jax.ops.index_update(
-        out,
-        jax.ops.index[(*idxs, slice(steps))],
-        jax.device_put(batch_out.reshape(*batch_shape, steps), jax.devices("cpu")[0])
-    )
+    for frame_idx in range(steps):
+        batch, batch_out = f_step(batch)
 
-for i in range(steps):
-    out_img = plt.cm.gist_rainbow(norm01(out[...,i]))[...,:3]
+        array_path = Path(workspace_dir, F"{template_name}_{frame_idx:06d}.npy")
+        out = np.load(array_path)
+        out = jax.ops.index_update(
+            out,
+            jax.ops.index[idxs],
+            batch_out.reshape(*batch_shape)
+        )
+        np.save(array_path, out)
+
+for frame_idx in range(steps):
+    array_path = Path(workspace_dir, F"{template_name}_{frame_idx:06d}.npy")
+    out = np.load(array_path)
+
+    out_img = plt.cm.gist_rainbow(norm01(out))[...,:3]
     out_img = (out_img * 255.99).astype(np.uint8)
 
-    out_dir = Path(F"/code/output/")
-    out_dir.mkdir(parents=True, exist_ok=True)
-
-    imsave(Path(out_dir, F"plane2d_2norm_loop100_{i:06d}.png"), out_img)
+    imsave(Path(out_dir, F"{template_name}_{frame_idx:06d}.png"), out_img)
