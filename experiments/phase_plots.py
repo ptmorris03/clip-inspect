6ccdd5e24aeabda644b4d6ecb4e86a24f61c8c64
pmorris2012 <pmorris2012@fau.edu>
2021-11-04 15:03:58 -0400

    exp

---
+++ experiments/phase_plots.py
@@ -0,0 +1,64 @@
+from clip_inspect.weights import load
+from clip_inspect.model import MLP
+from clip_inspect.generate import random_points
+from clip_inspect.transforms import loop, residual, loop_collect, loop_collect_residual
+from clip_inspect.inspect import norm01, polar_nd, angle_nd
+import jax.numpy as jnp
+import jax
+from tqdm import tqdm
+from jax import vmap, pmap, jit
+from functools import partial
+import matplotlib.pyplot as plt
+from matplotlib.colors import hsv_to_rgb
+from pathlib import Path
+import numpy as np
+from PIL import Image
+import os
+
+
+n_points = 10000
+pre_steps = 1000
+steps = 1000
+layer = 7
+
+
+#### LOAD THE MODEL
+state_dict, info = load("ViT-B-32", base_path="/code/weights/")
+mlp = MLP(state_dict, F"transformer.resblocks.{layer-1}")
+
+
+#### FUNCTIONS
+
+pre_fn = loop(mlp.forward, pre_steps)
+main_fn = loop_collect(mlp.forward, steps, angle_nd)
+
+@pmap
+@vmap
+@jit
+def f(point):
+    point = mlp.in_project(point)
+    point = pre_fn(point)
+    return main_fn(point)
+
+
+#### RUN
+points = random_points(mlp.prng_key, n_points, 512)
+coords = np.zeros((n_points, steps))
+batch = points.reshape(2, -1, 512)
+coords[:] = f(batch).reshape(n_points, steps)
+
+
+#### PLOT
+fig, ax = plt.subplots(figsize=(20, 20))
+c = np.linspace(0,1, steps - 1)
+for i in tqdm(range(n_points), "plot"):
+    plt.scatter(
+        coords[i,:-1],
+        coords[i,1:],
+        s=.1,
+        c=c
+    )
+out_path = Path("/code/output/phase_plots/")
+out_path.mkdir(exist_ok=True)
+fig.savefig(Path(out_path, F"polarnd_layer{layer}.png"))
+plt.close()
